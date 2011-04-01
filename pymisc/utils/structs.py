124a71b7a1c4dedf450bafd3c870d4be6471551d
Ilya Polosukhin <ilblackdragon@gmail.com>
2011-04-01 01:27:48 +0100

    Add some doctests to structs

--- pymisc/utils/structs.py
+++ pymisc/utils/structs.py
@@ -13,28 +13,57 @@ def Dict(**entries):
     return entries
 
 class DefaultDict(dict):
-    """Dictionary with a default value for unknown keys."""
+    """Dictionary with a default value for unknown keys.
+    >>> d = DefaultDict([])
+    >>> d[1].append(2)
+    >>> d
+    {1: [2]}
+    """
 
     def __init__(self, default):
         self.default = default
 
     def __getitem__(self, key):
+        """
+        >>> d = DefaultDict(0)
+        >>> d[1] += 1
+        >>> d[1]
+        1
+        """
         if key in self: return self.get(key)
         return self.setdefault(key, copy.deepcopy(self.default))
     
     def __copy__(self):
+        """
+        >>> d = DefaultDict(0)
+        >>> d[1] += 1
+        >>> p = d.copy()
+        >>> p
+        {1: 1}
+        """
         copy = DefaultDict(self.default)
         copy.update(self)
         return copy
     
 class RecursiveDict(dict):
-    """Dictionary witch contains dictionary and that continues n-times, plust has a default value for unknown keys at last dictionary."""
+    """Dictionary witch contains dictionary and that continues n-times, plust has a default value for unknown keys at last dictionary.
+    >>> d = RecursiveDict(2, [])
+    >>> d[1]['keyword'].append(5)
+    >>> d[1]['keyword']
+    [5]
+    """
 
     def __init__(self, ncount, default = None):
         self.ncount = ncount - 1
         self.default = default
 
     def __getitem__(self, key):
+        """
+        >>> d = RecursiveDict(2, [])
+        >>> d[1]['keyword'].append(5)
+        >>> d[1]['keyword']
+        [5]
+        """
         if key in self: 
            return self.get(key)
         if self.ncount > 0:
@@ -42,18 +71,42 @@ class RecursiveDict(dict):
         return self.setdefault(key, copy.deepcopy(self.default))
     
     def __copy__(self):
+        """
+        >>> d = RecursiveDict(2, [])
+        >>> d[1]['keyword'].append(5)
+        >>> p = d.copy()
+        >>> p
+        {1: {'keyword': [5]}}
+        """
         copy = RecursiveDict(self.ncount, self.default)
         copy.update(self)
         return copy
     
 class Struct:
     """Create an instance with argument=value slots.
-    This is for making a lightweight object whose class doesn't matter."""
+    This is for making a lightweight object whose class doesn't matter.
+    >>> a = Struct(a=1, b=2, c=3)
+    >>> a.a
+    1
+    >>> a.b
+    2
+    >>> a.c
+    3
+    """
 
     def __init__(self, **entries):
         self.__dict__.update(entries)
 
     def __cmp__(self, other):
+        """
+        >>> a = Struct(a=1, b=2, c=3)
+        >>> b = Struct(a=1, b=2, c=3)
+        >>> c = Struct(a=3, b=2, c=3)
+        >>> a == b
+        True
+        >>> a == c
+        False
+        """
         if isinstance(other, Struct):
             return cmp(self.__dict__, other.__dict__)
         else:
@@ -75,3 +128,7 @@ def update(x, **entries):
     else:
         x.__dict__.update(entries) 
     return x 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
