cabab3f4a1f38a93cc9e66600eea6db586150d5d
Illia Polosukhin <ilblackdragon@gmail.com>
2013-07-23 18:57:44 -0700

    Removed django subpackage (ilblackdragon/django-misc is used instead)

--- pymisc/django/middleware.py
+++
@@ -1,7 +0,0 @@
-from .utils import custom_spaceless
-
-class SpacelessMiddleware(object):
-    def process_response(self, request, response):
-        if 'text/html' in response['Content-Type']:
-            response.content = custom_spaceless(response.content)
-        return response
